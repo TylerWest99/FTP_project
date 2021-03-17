7a7e40887061c82042f7b5aac49c323e196bbe07
clinthof <73394309+clinthof@users.noreply.github.com>
2021-03-16 21:13:48 -0400

    Cleaned up a bit

--- ftp_client.py
+++ ftp_client.py
@@ -1,29 +1,22 @@
 #ftp_client
-import socket
+
+from socket import *
 import sys
 import os
 
-HOST = '123456789'  # The server's hostname or IP address
-PORT = 0       # The port used by the server
-
 status = 1
-
-# def connect(argv):
-#     Host = arg1
-#     PORT = arg2
 
 host = ""
 port = 0
-
 
 user_input = input("> ")
 ar1 = ""
 ar2 = ""
 ar3 = ""
 
-
 counter = 1
-spacesCounter = 0;
+spacesCounter = 0
+
 for i in range(len(user_input)):
     if(user_input[i] == " "):
         spacesCounter = spacesCounter + 1
@@ -39,39 +32,36 @@ if(spacesCounter == 2):
             counter = counter + 1
     ar3 = int(ar3)
 
-if (ar1 == "connect" or ar1 == "CONNECT" or ar1 == "C" or ar1 == "Connect" or ar1 == "c"):
+if (ar1.strip().upper() == "CONNECT" or ar1.strip().upper() == "C"):
     HOST = ar2
     PORT = ar3
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+    with socket(AF_INET, SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-    print("You have connected")
-    status = 1;
+        print("You have connected to\nHOST: %s\nPORT: %s" % (HOST, PORT))
+        status = 1
+
     while(status == 1):
         print()
         print("What would you like to do? You can List, Retrieve, Store, or Quit")
 
         op = input("> ")
 
-        if(op == "LIST" or op == "List" or op == "list" or op == "L" or op == "l"):
-            print("The files in the current directory are...")
-            print()
+        if(op.strip().upper() == "LIST" or op.strip().upper() == "L"):
+            print("The files in the current directory are...\n")
+            print("_________________________________________\n")
             # prints the files in the directory
             list = os.listdir('.')
+            count = 0
             for i in list:
                 print(i)
+                count += 1
+            print("Number of files found: ", count)
+            print("_________________________________________\n")
 
-        if(op == "QUIT" or op == "Quit" or op == "quit" or op == "Q" or op == "q"):
+        if(op.strip().upper() == "QUIT" or op.strip().upper() == "Q"):
             status = 0
+            s.close()
             print("Disconnected")
 else:
     print("Error in connecting")
-
-    #with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    #    s.connect((HOST, PORT))
-    #    val = input("Send or quit?")
-    #    if(val == "Send" or val == "s"):
-    #        s.sendall(b'1')
-    #        data = s.recv(1024)
-    #    if(val == "Quit" or val == "q"):
-    #        status = 0
-    #print('Received', repr(data))
